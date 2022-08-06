a714a2d6ffd13b4ca0c1ec51faa695190086c15e
LEE-MATTHEW <gom5714@naver.com>
2022-08-06 12:53:37 +0900

    react

---
+++ Button_DOM.md
@@ -0,0 +1,12 @@
+DOM
+<button onclick="myFunction()">CLick</button>
+JavaScript
+addEventListener("click", myFunction)
+
+Virtual DOM 
+<button onClick={myFunction}>CLick</button>
+
+JavaScript 연습 Vanilla JavaScript로 react 원리 파악해보기
+jQuery 안하는걸 추천!!
+Bootstrap5부터 jQuery를 제거
+NodeJS에서 jQuery가 없음
--- react/todo-app/src/App.js
+++ react/todo-app/src/App.js
@@ -66,27 +66,54 @@ export default function App(props) {
     })
     setTasks(editedTaskList)
   }
+  const taskList =tasks.filter(FILTER_MAP[filter]).map((task, index) => (
+    <Todo
+      key={task.id}
+      task={task}
+      deleteTask={deleteTask}
+      toggleTaskCompleted={toggleTaskCompleted}
+      editTask={editTask}
+    />
+  ))
+
+  const listHeading = useRef(null);
+  const prevTaskLength = useRef(null);
+
+  console.log("prevTaskLength",prevTaskLength)
+
+  // DOM에 접근하기 위해서 비동기로 작성한다
+  useEffect(()=> {
+    // 이전 렌더링의 prevTaskLength 값을 활용한다
+    if (tasks.length-prevTaskLength.current===-1) {
+      listHeading.current.focus()
+    }
+    // current 값을 업데이트 한다
+    prevTaskLength.current=tasks.length;
+  })
+
   return (
-    <>
+    <div className='px-2'>
       <Form addTask={addTask} />
+      <div className='flex flex-equal mb-2'>
       {FILTER_NAMES.map((name, index) => (
-        <button
-          key={index}
-          onClick={() => setFilter(name)}
-        >{name}</button>
-      ))}
+        <FilterButton 
+          key={name} 
+          name={name} 
+          setFilter={setFilter} 
+        />
+       ))}
+      </div>
+      <h2
+        tabIndex="-1" 
+        className=''
+        ref={listHeading}
+      >
+        {tasks.length} task(s) remaining
+      </h2>
       <ul>
-        {tasks.filter(FILTER_MAP[filter]).map((task, index) => (
-          <Todo
-            key={index}
-            task={task}
-            deleteTask={deleteTask}
-            toggleTaskCompleted={toggleTaskCompleted}
-            editTask={editTask}
-          />
-        ))}
+        {taskList}
       </ul>
-    </>
+    </div>
   );
 }
 function Form(props) {
@@ -115,43 +142,106 @@ function Form(props) {
 
   return (
     <>
-      <h1>할일 목록</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name='' value={name} onChange={handleChange} />
-        <button type="submit">Add</button>
+      
+      <form onSubmit={handleSubmit} className="mb-2">
+        <h1>할일 목록</h1>
+        <input 
+          type="text" 
+          name='' 
+          className='w-full mb-2'
+          value={name} 
+          onChange={handleChange} 
+        />
+        <button 
+          type="submit"
+          className='btn btn-secondary w-full'
+        >Add</button>
       </form>
     </>
   )
 }
-
+function FilterButton(props) {
+  return(
+    <button 
+      type='button' 
+      className='btn btn-light'
+      onClick={()=> props.setFilter(props.name)}
+    >
+      {props.name}
+    </button>
+  ) 
+}
 function Todo(props) {
   console.log("Todo Loaded", props.task)
   
   const [isEditing, setIsEditing] =useState(false);
   const [newName, setNewName] =useState("");
+  const inputEl = useRef(null);
+  const editButton = useRef(null);
+  const wasEditing = useRef(null);
+
+  console.log("isEditing", isEditing)
+  console.log("wasEditing", wasEditing)
+  // useRef를 사용해 edit버튼을 눌렀을경우 input으로 커서가 가게하는 법
+  // editingTemplate에서 input에 focus효과를 준다
+  useEffect(()=>{
+    if (isEditing) {
+      inputEl.current.focus()
+    }
+    // wasEditing값에 따라 input에 focus를 결정한다
+    if(wasEditing.current) {
+      editButton.current.focus();
+    }
+    // current 값을 변경한다
+    // 다시 랜더링을 호출하지는 않는다
+    wasEditing.current = isEditing;
+  })
 
   function handleSubmit(e) {
     e.preventDefault();
+    
+    if(!newName.trim()) { // 빈 String은 조건에서 false이다
+      return;
+    }
     props.editTask(props.task.id, newName)
     // 수정이 완료되면 해당 Todo를 다시 viewTemplate으로 변경한다
     // 이 기능은 없으면 초기 상태로 다시 돌아가는 것이 아니라 그상태가 유지되어 초기화면으로 돌아가지지 않는다
     setIsEditing(false);
+    setNewName("");
   }
   function handleChange(e) {
     setNewName(e.target.value);
+    
   }
 
   const viewTemplate = (
-    <div>
-      <input type="checkbox"
-        defaultChecked={props.task.completed}
-        onChange={() => props.toggleTaskCompleted(props.task.id)}
-      />
-      {props.task.name}
-      <br />
-      <button onClick={()=> setIsEditing(true)}>Edit</button>
-      <button onClick={() => props.deleteTask(props.task.id)}>Delete</button>
-    </div>
+    <>
+      <div className='flex items-center' style={{height:"75px"}}>
+        <input type="checkbox"
+          defaultChecked={props.task.completed}
+          onChange={() => props.toggleTaskCompleted(props.task.id)}
+        />
+        <label htmlFor='' className='ml-2'>
+          {props.task.name}
+        </label>
+      </div>
+      <div className='flex flex-equal'>
+        <button 
+          className='btn btn-light'
+          onClick={()=> setIsEditing(true)}
+          ref={editButton}
+        >
+          Edit
+        </button>
+        <button 
+          className='btn btn-danger'
+          onClick={() => props.deleteTask(props.task.id)}
+        >
+          Delete
+        </button>
+
+      </div>
+    </>
   )
 
   // button type
@@ -159,10 +249,38 @@ function Todo(props) {
   // button : form내에서 button의 역할(기능을 주어야함)
   const editingTemplate = (
     <form onSubmit={handleSubmit}>
-      <input type="text" onChange={handleChange}/>
-      <br/>
-      <button type="submit">Save</button>
-      <button type="button" onClick={()=>setIsEditing(false)}>Cancel</button>
+      {/* form group */}
+      <div className='flex items-center' style={{height:"75px"}}>
+        <div className='w-full'>
+          <label htmlFor=''>New name for {props.task.name}</label>
+          <input 
+            type="text"
+            className='w-full' 
+            onChange={handleChange} 
+            ref={inputEl}
+          />
+        </div>
+      </div>
+      
+      {/* button group */}
+      <div className='flex flex-equal'>
+        <button 
+          type="button"
+          className='btn btn-light' 
+          onClick={()=>{
+            setIsEditing(false);
+            setNewName("");
+          }}
+        >
+          Cancel
+        </button>
+        <button 
+          type="submit"
+          className='btn btn-secondary'
+          >
+          Save
+        </button>
+      </div>
     </form>
   )
   return (
